06f1d4d15a29f10ab0a72f41984efba83137792a
pekkao <pekka.ojala@oamk.fi>
2023-09-25 21:16:27 +0300

    Basic version

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View, Button } from 'react-native';
+
+const URL = "https://www.boredapi.com/api/activity";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [activity, setActivity] = useState("");
+  const [type, setType] = useState("");
+  const [participants, setParticipants] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [refresh, setRefresh] = useState();
+  
+  useEffect(()=> {
+    fetch(URL)
+      .then(response => response.json())
+      .then ((json) => {
+        console.log(json);
+        setActivity(firstLetterToLowerCase(json.activity));
+        setType(json.type);
+        setParticipants(json.participants);
+        setPrice(json.price);
+        setError(null);
+        setIsLoading(false);
+      },(error) => {
+        setError("Error retrieving activity!");
+        setIsLoading(false);
+        console.log(error);
+      })
+  },[refresh])
+
+  const firstLetterToLowerCase = (str) => {
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  }
+
+  const getNewActivity = () => {
+    setRefresh({});
+  }
+
+
+  if (isLoading) {
+    return <View style={styles.container}><ActivityIndicator size="large"/></View>
+  } else if (error) {
+    return <View style={styles.container}><Text>{error}</Text></View>
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>I'm so bored...</Text>
+        <Text style={styles.activity}>As a {type} type of activity {activity}. 
+        It takes {participants} participant(s) and
+        {price === 0 ? ' does not cost anything.': ' is probably quite cheap.'}
+        </Text>
+        <Button title="Refresh" onPress={() => getNewActivity()}></Button>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +63,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 20
   },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginBottom: 20,
+  },  
+  activity: {
+    marginBottom: 10,
+  }
 });
